943131b132124eb23e7f9cd551b17ba22d3daa12
Sebastian Villalobos <sebastian.villalobos@scaleapi0.onmicrosoft.com>
2022-05-23 00:34:30 -0500

    Added docstring to each function

--- Functions.py
+++ Functions.py
@@ -15,6 +15,19 @@ import numpy as np
 import pandas as pd
 
 def resize_Array(arr1, arr2):
+    '''
+    Resize array to match shape of another array, 
+    either by adding rows or columns of 0's,
+    or removing them
+
+            Parameters:
+                    arr1 (np.ndarray): Array to reshape
+                    arr2 (np.ndarray): Array whose shape we will match
+
+            Returns:
+                    arr1 (np.ndarray): Array that matches arr2's shape
+    '''
+
     if arr1.shape[0] < arr2.shape[0]:
         arr1 = np.append(arr1, np.zeros((arr2.shape[0]-arr1.shape[0], ), dtype=int))
     elif arr1.shape[0] > arr2.shape[0]:
@@ -29,6 +42,19 @@ def resize_Array(arr1, arr2):
     
 
 def create_Rectangle(height, width):
+
+    '''
+    Create a matrix of the form (height, width), that represents a rectangle.
+    With ts perimeter as 1's and everything else as 0's
+
+            Parameters:
+                    height (int): Height of the rectangle
+                    width (int): Width of the rectangle
+
+            Returns:
+                    rect (np.ndarray): Array that contains the rectangle perimeter
+    '''
+
     rect = rectangle(height, width)*0
     rect[0:2,:] = True
     rect[-2:,:] = True
@@ -38,6 +64,16 @@ def create_Rectangle(height, width):
     return rect
 
 def plot_images(original_image, filtered_image):
+
+    '''
+    Plot two images side by side
+
+            Parameters:
+                    original_image (np.ndarray): First image
+                    filtered_image (np.ndarray): Second image
+
+    '''
+
     fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(25, 25),
                          sharex=True, sharey=True)
 
@@ -55,12 +91,44 @@ def plot_images(original_image, filtered_image):
     plt.show()
 
 def get_API_tasks(key, project):
+
+    '''
+    Get all tasks from a project using ScaleAPI
+
+            Parameters:
+                    key (string): Users API key
+                    project (string): Name of the project on Scale's platform
+
+            Returns:
+                    list(tasks) (list): List that contains the project's tasks
+    '''    
+
     client = scaleapi.ScaleClient(key)
     tasks = client.get_tasks(project_name = project)
     
     return list(tasks)
 
 def audit_task(task_list, num_annotation=2, plot=False, dilation_=False, sigma=1, threshold=0.2):
+
+    '''
+    Grab a task at random and obtain an specific number of annotations to check
+    Use a Canny filter to obtain the edges of each annotation and create certain figures (rectangle, diamond, disk),
+    which is the shape of most street signs, of the size of the bounding box
+    Sum these matrices and see if any of these shapes are contained in the part of the image delimited by its 
+    annotation bounding box
+
+            Parameters:
+                    task_list (list): List containing the tasks of a project
+                    num_annotation (int): Number of annotations to check
+                    plot (bool): Flag used to plot the images or not
+                    dilation_ (bool): Flag used to do a dilation to the task's image
+                    sigma (int): Value used for the canny filter. Max value is 3
+                    threshold (float): Limit to use to decide if we have warnings and errors
+
+            Returns:
+                    CSV containing the annotation uuid, label, the confidence for each shape, warning and error
+    '''  
+
     df = pd.DataFrame(columns=['uuid', 'label', 'rect_conf', 'dia_conf', 'dis_conf', 'warning', 'error'])
     
     task_list_copy = task_list.copy()
